b262d71b290eb644566bf7c7bbf31894ea14bb7b
“MaHu4” <marenhuser@gmail.com>
2022-04-07 13:14:45 +0545

    google maps added to contact section

--- custom.js
+++ custom.js
@@ -1,8 +1,11 @@
 
- function myMap() {
-    var mapProp = {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
+  function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     }
+
+
+
