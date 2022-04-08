4d067346f45e78035d7a6a0e16ab1aead77971f2
“MaHu4” <marenhuser@gmail.com>
2022-04-08 13:23:40 +0545

    first JS function added for sumbit button in ciontact-section

--- custom.js
+++ custom.js
@@ -1,16 +1,22 @@
 
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com',
-            'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
-        }
-    };
-    
-    fetch('https://trueway-places.p.rapidapi.com/FindPlaceByText?text=Buddha%20chillout%20rooftop%20bar%20Lumbini&language=en', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+
+
+/* contact- field
+
+   change text of submit button */
+
+const btn = document.getElementById('submitButton');
+
+// ✅ Change text of button text on click
+btn.addEventListener('click', function handleClick() {
+  const initialText = '.submit';
+
+  btn.innerHTML = `<span>.thank you!<span>`;
+});
+// change style of new text
+document.getElementById("submitButton").style.color = "#005580";
+document.getElementById("submitButton").style.fontFamily = "'Merriweather', serif;";
+document.getElementById("submitButton").style.fontSize = "36px";
 
 
