a05dac1c91344d3cb28f9512d1fd86b62a8d631e
“MaHu4” <marenhuser@gmail.com>
2022-04-10 21:15:32 +0545

    editing of nad yog page with promo section coloumn right

--- nadyog.js
+++ nadyog.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("July, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
@@ -17,12 +17,15 @@ var countdownfunction = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
   
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(countdownfunction);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = ".if you don't see our new webpage here, it will come later : )";
   }
 }, 1000);
+
+
+
